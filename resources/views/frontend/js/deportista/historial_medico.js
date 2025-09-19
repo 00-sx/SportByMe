6d18de5445d324eb766ec16100ffd95cbfe47f7c
00-sx <17998663700l@gmail.com>
2025-09-19 16:49:58 -0500

    Limpieza: aplicar .gitignore

--- resources/views/frontend/js/deportista/historial_medico.js
+++
@@ -1,7 +0,0 @@
-// Lógica para el Acordeón
-        document.querySelectorAll('.accordion-item').forEach(item => {
-            const header = item.querySelector('.accordion-header');
-            header.addEventListener('click', () => {
-                item.classList.toggle('active');
-            });
-        });
--- resources/views/frontend/js/deportista/rendimiento.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.accordion-item').forEach(item => {
-            const header = item.querySelector('.accordion-header');
-            header.addEventListener('click', () => {
-                item.classList.toggle('active');
-            });
-        });
